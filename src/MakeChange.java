17f67326ed70c59b3504dcc19cc834ff84b4aacb
Khiveley <kenneth.hiveley@gmail.com>
2021-07-23 21:17:02 -0500

    Snarky attendant- keyboard variables and proposed currency variables with //

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		
+		Double price = 0.0;
+		Double cashReceived = 0.0;
+//		Double tenDollar = 10.0;
+//		Double fiveDollar = 5.0;
+//		Double dollar = 1.0;
+//		Double quarter = 0.25;
+//		Double dime = 0.10;
+//		Double nickel = 0.05;
+//		Double penny = 0.01.
+		
+		System.out.println("Hello! Looks like you're ready to checkout. What is the price of your item? ");
+		price = keyboard.nextDouble();
+		
+		System.out.println("Our card reader is down. How much cash do you have? ");
+		cashReceived = keyboard.nextDouble();
+			if (cashReceived < price) {
+		System.out.println("You'll need more money or you'll have to put the item back. ");	
+			}
+		else if (cashReceived > price) {
+			System.out.println("Let me get you your change. ");
 
 	}
 
